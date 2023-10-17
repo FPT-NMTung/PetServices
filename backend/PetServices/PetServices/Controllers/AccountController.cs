9bcd1cb82f1579997cf6bc9af758eb6e8419833f
hungnvhe153434 <hungnvhe153434@fpt.edu.vn>
2023-10-18 00:53:40 +0700

    fix comment

--- backend/PetServices/PetServices/Controllers/AccountController.cs
+++ backend/PetServices/PetServices/Controllers/AccountController.cs
@@ -53,10 +53,10 @@ namespace PetServices.Controllers
             if (result != null)
             {
                 var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, login.Email),
-            new Claim(ClaimTypes.Role, result.Role?.RoleName) // Lưu tên vai trò (RoleName) vào mã thông báo
-        };
+            {
+                new Claim(ClaimTypes.Name, login.Email),
+                new Claim(ClaimTypes.Role, result.Role?.RoleName) // Lưu tên vai trò (RoleName) vào mã thông báo
+            };
 
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
                 var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -77,7 +77,6 @@ namespace PetServices.Controllers
             }
         }
 
-
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterDTO registerDto)
         {
@@ -85,7 +84,6 @@ namespace PetServices.Controllers
             {
                 return BadRequest(ModelState);
             }
-
             // Kiểm tra Email
             if (!IsValidEmail(registerDto.Email))
             {
@@ -100,36 +98,30 @@ namespace PetServices.Controllers
                 return BadRequest(ModelState);
             }
 
-            // Kiểm tra xem tài khoản đã tồn tại dựa trên UserName
             if (_context.Accounts.Any(a => a.Email == registerDto.Email))
             {
                 return Conflict("Email đã được đăng ký");
             }
 
-            // Tạo một đối tượng Account từ thông tin đăng ký (email và password)
             var newAccount = new Account
             {
                 Email = registerDto.Email,
                 Password = registerDto.Password,
-                Status = false, // Hoặc giá trị mặc định khác tùy thuộc vào yêu cầu
+                Status = false, 
                 RoleId = 2
             };
 
-            // Thêm Account vào cơ sở dữ liệu
             await _context.Accounts.AddAsync(newAccount);
             await _context.SaveChangesAsync();
 
-            // Gán thông tin UserInfo cho Account
             newAccount.UserInfo = new UserInfo
             {
-                // Thiết lập thông tin UserInfo tại đây
                 FirstName = registerDto.FirstName,
                 LastName = registerDto.LastName,
                 Phone = registerDto.Phone,
                 Address = registerDto.Address
             };
 
-            // Lưu thông tin UserInfo vào cơ sở dữ liệu
             _context.Update(newAccount);
             await _context.SaveChangesAsync();
 
@@ -137,21 +129,18 @@ namespace PetServices.Controllers
         }
 
         private bool IsValidEmail(string email)
-        {
-            // Sử dụng Regular Expression để kiểm tra định dạng Email
+        {          
             string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
             return Regex.IsMatch(email, emailPattern);
         }
 
         private bool IsValidPassword(string password)
-        {
-            // Kiểm tra độ dài và các quy tắc khác cho mật khẩu
-            return !string.IsNullOrWhiteSpace(password) && password.Length >= 8 && !password.Contains(" "); // Kiểm tra không chứa khoảng trắng
+        {            
+            return !string.IsNullOrWhiteSpace(password) && password.Length >= 8 && !password.Contains(" "); 
         }
 
         private bool IsValidPhone(string phone)
         {
-            // Kiểm tra độ dài và định dạng số điện thoại
             return !string.IsNullOrWhiteSpace(phone) && phone.Length == 10 && phone.StartsWith("0") && phone.All(char.IsDigit);
         }
 
@@ -183,30 +172,25 @@ namespace PetServices.Controllers
                 ModelState.AddModelError("Số điện thoại không hợp lệ", "Số điện thoại bắt đầu bằng số 0 và có tổng 10 số");
                 return BadRequest(ModelState);
             }
-
-            // Kiểm tra xem tài khoản đã tồn tại dựa trên UserName
+            
             if (_context.Accounts.Any(a => a.Email == registerDto.Email))
             {
                 return Conflict("Email đã được đăng ký");
             }
-
-            // Tạo một đối tượng Account từ thông tin đăng ký (email và password)
+            
             var newAccount = new Account
             {
                 Email = registerDto.Email,
                 Password = registerDto.Password,
-                Status = false, // Hoặc giá trị mặc định khác tùy thuộc vào yêu cầu
+                Status = false, 
                 RoleId = 4
             };
 
-            // Thêm Account vào cơ sở dữ liệu
             await _context.Accounts.AddAsync(newAccount);
             await _context.SaveChangesAsync();
 
-            // Gán thông tin UserInfo cho Account
             newAccount.PartnerInfo = new PartnerInfo
             {
-                // Thiết lập thông tin UserInfo tại đây
                 FirstName = registerDto.FirstName,
                 LastName = registerDto.LastName,
                 Phone = registerDto.Phone,
@@ -216,7 +200,6 @@ namespace PetServices.Controllers
                 
             };
 
-            // Lưu thông tin UserInfo vào cơ sở dữ liệu
             _context.Update(newAccount);
             await _context.SaveChangesAsync();
 
@@ -259,8 +242,7 @@ namespace PetServices.Controllers
                 }
             }
             catch (Exception ex)
-            {
-                // Xử lý các trường hợp lỗi một cách thích hợp và trả về mã lỗi phù hợp
+            {               
                 return BadRequest("An error occurred.");
             }
         }
@@ -302,7 +284,6 @@ namespace PetServices.Controllers
             }
             catch (Exception ex)
             {
-                // Handle errors appropriately and return the appropriate error code
                 return BadRequest("Đã xảy ra lỗi.");
             }
         }
