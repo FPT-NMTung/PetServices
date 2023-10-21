2600612a01b9ce6f208dea008cbe34edb632f166
TrungTM <trungtmhe151045@fpt.edu.vn>
2023-10-21 17:49:28 +0700

    add roomcategory

--- forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/RoomCategoryController.cs
+++ forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/RoomCategoryController.cs
@@ -11,10 +11,10 @@ namespace FEPetServices.Areas.Manager.Controllers
     {
         private readonly HttpClient client = null;
         private string DefaultApiUrl = "";
-        private string DefaultApiUrlRoomCategoryList = "";
-        private string DefaultApiUrlRoomCategoryDetail = "";
-        private string DefaultApiUrlRoomCategoryAdd = "";
-        private string DefaultApiUrlRoomCategoryUpdate = "";
+        private string ApiUrlRoomCategoryList = "";
+        private string ApiUrlRoomCategoryDetail = "";
+        private string ApiUrlRoomCategoryAdd = "";
+        private string ApiUrlRoomCategoryUpdate = "";
 
         public RoomCategoryController()
         {
@@ -22,10 +22,10 @@ namespace FEPetServices.Areas.Manager.Controllers
             var contentType = new MediaTypeWithQualityHeaderValue("application/json");
             client.DefaultRequestHeaders.Accept.Add(contentType);
             DefaultApiUrl = "";
-            DefaultApiUrlRoomCategoryList = "https://localhost:7255/api/RoomCategory/GetAllRoomCategory";
-            DefaultApiUrlRoomCategoryDetail = "https://localhost:7255/api/RoomCategory/GetRoomCategory";
-            DefaultApiUrlRoomCategoryAdd = "https://localhost:7255/api/RoomCategory/AddRoomCategory";
-            DefaultApiUrlRoomCategoryUpdate = "https://localhost:7255/api/RoomCategory/UpdateRoomCategory?roomCategoryId=";
+            ApiUrlRoomCategoryList = "https://localhost:7255/api/RoomCategory/GetAllRoomCategory";
+            ApiUrlRoomCategoryDetail = "https://localhost:7255/api/RoomCategory/GetRoomCategory";
+            ApiUrlRoomCategoryAdd = "https://localhost:7255/api/RoomCategory/AddRoomCategory";
+            ApiUrlRoomCategoryUpdate = "https://localhost:7255/api/RoomCategory/UpdateRoomCategory?roomCategoryId=";
 
         }
 
@@ -36,7 +36,7 @@ namespace FEPetServices.Areas.Manager.Controllers
                 var json = JsonConvert.SerializeObject(roomCategoryDTO);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                HttpResponseMessage response = await client.GetAsync(DefaultApiUrlRoomCategoryList);
+                HttpResponseMessage response = await client.GetAsync(ApiUrlRoomCategoryList);
                 if (response.IsSuccessStatusCode)
                 {
                     var responseContent = await response.Content.ReadAsStringAsync();
@@ -63,50 +63,50 @@ namespace FEPetServices.Areas.Manager.Controllers
             return View();
         }
 
-        /*public async Task<ActionResult> AddServiceCategory([FromForm] ServiceCategoryDTO serviceCategory, IFormFile image)
+        public async Task<ActionResult> AddRoomCategory([FromForm] RoomCategoryDTO roomCategoryDTO, IFormFile image)
         {
             try
             {
-                if (ModelState.IsValid) // Kiểm tra xem biểu mẫu có hợp lệ không
+                if (ModelState.IsValid)
                 {
                     if (image != null && image.Length > 0)
                     {
-                        // Xử lý và lưu trữ ảnh
                         Console.WriteLine(image);
-                        serviceCategory.Picture = "/img/" + image.FileName.ToString();
+                        roomCategoryDTO.Picture = "/img/" + image.FileName.ToString();
                     }
 
-                    var json = JsonConvert.SerializeObject(serviceCategory);
+                    roomCategoryDTO.Status = true;
+
+                    var json = JsonConvert.SerializeObject(roomCategoryDTO);
                     var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                    // Gửi dữ liệu lên máy chủ
-                    HttpResponseMessage response = await client.PostAsync(DefaultApiUrlServiceCategoryAdd, content);
+                    HttpResponseMessage response = await client.PostAsync(ApiUrlRoomCategoryAdd, content);
 
                     if (response.IsSuccessStatusCode)
                     {
-                        TempData["SuccessMessage"] = "Thêm dịch vụ thành công!";
-                        return View(serviceCategory); // Chuyển hướng đến trang thành công hoặc trang danh sách
+                        TempData["SuccessMessage"] = "Thêm loại phòng thành công!";
+                        return View(roomCategoryDTO);
                     }
                     else
                     {
-                        ViewBag.ErrorMessage = "Thêm dịch vụ thất bại. Vui lòng thử lại sau.";
-                        return View(serviceCategory); // Hiển thị lại biểu mẫu với dữ liệu đã điền
+                        ViewBag.ErrorMessage = "Thêm loại phòng thất bại. Vui lòng thử lại sau!";
+                        return View(roomCategoryDTO);
                     }
                 }
                 else
                 {
-                    return View(serviceCategory);
+                    return View(roomCategoryDTO);
                 }
             }
             catch (Exception ex)
             {
                 ViewBag.ErrorMessage = "Đã xảy ra lỗi: " + ex.Message;
-                return View(serviceCategory); // Hiển thị lại biểu mẫu với dữ liệu đã điền
+                return View(roomCategoryDTO);
             }
         }
 
 
-
+        /*
         [HttpGet]
         public async Task<IActionResult> EditServiceCategory(int serCategoriesId)
         {
