6f9636336bf79f43cab0b8956ec8093d5a93c6f7
RoyTun <hungnphe150914@fpt.edu.vn>
2023-10-25 18:34:19 +0700

    update list part in role Manager

---
+++ forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/ListPartnerController.cs
@@ -0,0 +1,93 @@
+﻿using FEPetServices.Form;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace FEPetServices.Areas.Manager.Controllers
+{
+    [Authorize(Policy = "ManaOnly")]
+    public class ListPartnerController : Controller
+    {
+        private readonly HttpClient _client = null;
+        private string DefaultApiUrl = "";
+
+        public ListPartnerController()
+        {
+            _client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _client.DefaultRequestHeaders.Accept.Add(contentType);
+            DefaultApiUrl = "https://localhost:7255/api/Partner";
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await _client.GetAsync(DefaultApiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                List<AccountInfo> listAccounts = System.Text.Json.JsonSerializer.Deserialize<List<AccountInfo>>(responseContent, options);
+                return View(listAccounts);
+            }
+            else
+            {
+                TempData["ErrorLoadingDataToast"] = "Lỗi hệ thống vui lòng thử lại sau";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DetailPartner(string email)
+        {
+
+            HttpResponseMessage response = await _client.GetAsync(DefaultApiUrl + "/" + email);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                AccountInfo accountInfo = System.Text.Json.JsonSerializer.Deserialize<AccountInfo>(responseContent, options);
+                return View(accountInfo);
+            }
+            else {
+                TempData["ErrorLoadingDataToast"] = "Lỗi hệ thống vui lòng thử lại sau";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DetailPartner()
+        {
+            /* HttpResponseMessage response = await _client.GetAsync(DefaultApiUrl + "/" + email);
+             if (response.IsSuccessStatusCode)
+             {
+                 string responseContent = await response.Content.ReadAsStringAsync();
+
+                 var options = new JsonSerializerOptions
+                 {
+                     PropertyNameCaseInsensitive = true
+                 };
+
+                 AccountInfo accountInfo = System.Text.Json.JsonSerializer.Deserialize<AccountInfo>(responseContent, options);
+                 return View(accountInfo);
+             }
+             else
+             {
+                 TempData["ErrorLoadingDataToast"] = "Lỗi hệ thống vui lòng thử lại sau";
+                 return View();
+             }*/
+            return View();
+        }
+    }
+}
