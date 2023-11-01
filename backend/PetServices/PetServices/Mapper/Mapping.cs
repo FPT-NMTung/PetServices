0f7c73172ca7bf000600b4c5f61664a378a94442
hungnvhe153434 <hungnvhe153434@fpt.edu.vn>
2023-11-01 23:09:01 +0700

    validate product prepare test

--- backend/PetServices/PetServices/Mapper/Mapping.cs
+++ backend/PetServices/PetServices/Mapper/Mapping.cs
@@ -113,7 +113,7 @@ namespace PetServices.Mapper
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
                 .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
                 .ForMember(dest => dest.CreateDate, opt => opt.MapFrom(src => src.CreateDate))
-                .ForMember(dest => dest.Quanlity, opt => opt.MapFrom(src => src.Quantity))
+                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
                 .ForMember(dest => dest.ProCategoriesId, opt => opt.MapFrom(src => src.ProCategoriesId))
                 .ForMember(dest => dest.ProCategoriesName, opt => opt.MapFrom(src => src.ProCategories.ProCategoriesName));
 
