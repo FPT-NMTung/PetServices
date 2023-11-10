dd025a38b60b1be026f35c8f6a1a1a761529b78c
ngangiang2909 <ngangiang2909@gmail.com>
2023-11-10 22:33:34 +0700

    api partner order

---
+++ backend/PetServices/PetServices/Controllers/OrderPartnerController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PetServices.DTO;
+using PetServices.Models;
+
+namespace PetServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderPartnerController : ControllerBase
+    {
+        private PetServicesContext _context;
+        private IMapper _mapper;
+        private readonly IConfiguration _configuration;
+
+        public OrderPartnerController(PetServicesContext context, IMapper mapper, IConfiguration configuration)
+        {
+            _context = context;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+        [HttpGet("ListOrderPetTraining")]
+        public async Task<IActionResult> ListOrderPetTraining(int serCategoriesId)
+        {
+            List<Order> orders = await _context.Orders
+                .Include(x => x.BookingServicesDetails)
+                .ThenInclude(y => y.Service)
+                .Where(o => o.BookingServicesDetails.Any(b =>
+                    b.Service != null &&
+                    b.Service.Status == true &&
+                    b.Service.SerCategoriesId == serCategoriesId))
+                .ToListAsync();
+
+            return Ok(_mapper.Map<List<OrdersDTO>>(orders));
+        }
+
+    }
+}
--- forntend/FEPetServices/FEPetServices/Areas/Partner/Controllers/HomePartnerController.cs
+++ forntend/FEPetServices/FEPetServices/Areas/Partner/Controllers/HomePartnerController.cs
@@ -1,12 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FEPetServices.Controllers;
+using FEPetServices.Form.BookingForm;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 
 namespace FEPetServices.Areas.Partner.Controllers
 {
     public class HomePartnerController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient client = null;
+        private string DefaultApiUrl = "";
+        private string DefaultApiUrlOrderListOfPetTraining = "";
+        private string DefaultApiUrlOrderListOfPetTrainingSpecial = "";
+        public HomePartnerController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            DefaultApiUrl = "";
+            DefaultApiUrlOrderListOfPetTraining = "";
+            DefaultApiUrlOrderListOfPetTrainingSpecial = "";
+        }
+        public async Task<IActionResult> Index()
         {
             return View();
         }
+        //public async Task<IActionResult> ListOrderPetTraining()
+        //{
+        //    // Gọi API để lấy danh sách đơn đặt hàng pet training
+        //    List<OrderForm> petTrainingOrders = await GetPetTrainingOrdersAsync();
+
+        //    // Truyền danh sách đơn đặt hàng vào view
+        //    return View(petTrainingOrders);
+        //}
+
+        //private async Task<List<OrderForm>> GetPetTrainingOrdersAsync()
+        //{
+        //    List<OrderForm> petTrainingOrders = new List<OrderForm>();
+
+        //    try
+        //    {
+        //        // Gọi API để lấy danh sách đơn đặt hàng dựa trên pet training
+        //        HttpResponseMessage response = await client.GetAsync(DefaultApiUrlOrderListOfPetTraining);
+
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            // Chuyển đổi nội dung response thành danh sách đơn đặt hàng
+        //            petTrainingOrders = await response.Content.ReadFromJsonAsync<List<OrderForm>>();
+        //        }
+        //        else
+        //        {
+        //            // Xử lý lỗi nếu cần
+        //            ModelState.AddModelError(string.Empty, "Error retrieving pet training orders.");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Xử lý lỗi nếu cần
+        //        ModelState.AddModelError(string.Empty, "An error occurred while processing your request.");
+        //    }
+
+        //    return petTrainingOrders;
+        //}
+        //public List<OrderForm> GetOrdersByServiceCategory(string serviceCategory)
+        //{
+        //    var orders = new List<OrderForm>();
+        //    // Lấy danh sách đơn hàng
+        //    // ...
+        //    // Lọc danh sách đơn hàng theo danh mục dịch vụ
+        //    var filteredOrders = orders.Where(order => order.BookingServicesDetails.Any(service => service.Service.SerCategoriesName == serviceCategory)).ToList();
+        //    return filteredOrders;
+        //}
     }
 }
