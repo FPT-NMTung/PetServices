ff112fa88663620d82d870093d46a95563ae2e5a
TrungTM <trungtmhe151045@fpt.edu.vn>
2023-11-20 11:43:27 +0700

    update formate, css date + logic feedback

--- backend/PetServices/PetServices/Controllers/FeedbackController.cs
+++ backend/PetServices/PetServices/Controllers/FeedbackController.cs
@@ -27,5 +27,61 @@ namespace PetServices.Controllers
             List<Feedback> blogs = _context.Feedbacks.ToList();
             return Ok(_mapper.Map<List<FeedbackDTO>>(blogs));
         }
+
+        [HttpGet("GetInfomationForFeedback")]
+        public IActionResult GetInfomationForFeedback()
+        {
+            return Ok();
+        }
+
+        [HttpPost("AddFeedBack")]
+        public async Task<ActionResult> AddFeedBack(FeedbackDTO feedbackDTO)
+        {
+            if(string.IsNullOrWhiteSpace(feedbackDTO.Content))
+            {
+                string errorMessage = "Nội dung không được để trống!";
+                return BadRequest(errorMessage);
+            }
+            if (feedbackDTO.Content.Length > 500)
+            {
+                string errorMessage = "Nội dung không được viết dưới 20 kí tự!";
+                return BadRequest(errorMessage);
+            }
+            if (string.IsNullOrEmpty(feedbackDTO.NumberStart.ToString()))
+            {
+                string errorMessage = "Số sao không được để trống!";
+                return BadRequest(errorMessage);
+            }
+            if (feedbackDTO.NumberStart < 1 && feedbackDTO.NumberStart > 5)
+            {
+                string errorMessage = "Số sao không được vượt quá 0-5!";
+                return BadRequest(errorMessage);
+            }
+
+            try
+            {
+                var feedback = new Feedback
+                {
+                    Content = feedbackDTO.Content,
+                    NumberStart = feedbackDTO.NumberStart,
+                    ServiceId = feedbackDTO.ServiceId,
+                    RoomId = feedbackDTO.RoomId,
+                    PartnerId = feedbackDTO.PartnerId,
+                    ProductId = feedbackDTO.ProductId,
+                    UserId = feedbackDTO.UserId,
+                    OrderId = feedbackDTO.OrderId,
+                };
+
+                await _context.Feedbacks.AddAsync(feedback);
+                await _context.SaveChangesAsync();
+
+                return Ok("Thêm đánh giá thành công!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Đã xảy ra lỗi: {ex.Message}");
+            }
+        }
+
     }
 }
