bd979197b2f717741be88957cd1297c3aef85f1c
hungnvhe153434 <hungnvhe153434@fpt.edu.vn>
2023-11-23 01:29:01 +0700

    fix conflig

--- forntend/FEPetServices/FEPetServices/Controllers/LogoutController.cs
+++ forntend/FEPetServices/FEPetServices/Controllers/LogoutController.cs
@@ -8,13 +8,10 @@ namespace FEPetServices.Controllers
     {
         public IActionResult Index()
         {
-            // Kiểm tra xem người dùng đã được xác thực (authenticated) hay chưa
             if (User.Identity.IsAuthenticated)
             {
-                // Đăng xuất khỏi hệ thống
                 HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-                // Xóa thông tin phiên làm việc (session)
                 HttpContext.Session.Remove("UserName");
                 HttpContext.Session.Remove("UserImage");
                 HttpContext.Session.Clear();
