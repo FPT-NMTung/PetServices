46de980bb8f09bc68ffba8f0b26be349cfba9414
TrungTM <trungtmhe151045@fpt.edu.vn>
2023-11-24 19:22:58 +0700

    FE Demo DashBoard

---
+++ forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FEPetServices.Areas.Manager.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
