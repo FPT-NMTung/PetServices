d0107c4ac6f17c91c8e5f5564824c20b8f2061c8
TrungTM <trungtmhe151045@fpt.edu.vn>
2023-12-02 02:20:37 +0700

    update code

--- forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/DashBoardController.cs
+++ forntend/FEPetServices/FEPetServices/Areas/Manager/Controllers/DashBoardController.cs
@@ -190,35 +190,6 @@ namespace FEPetServices.Areas.Manager.Controllers
                     ViewBag.Top5CustomerArea = new SelectList(Top5CustomerArea, "date", "quantity");
                 }
 
-                // đánh giá của khách hàng về các sản phẩm
-                HttpResponseMessage FeedbackOfProductResponse = await client.GetAsync("https://localhost:7255/api/Dashboard/GetFeedbackOfProduct");
-
-                if (FeedbackOfProductResponse.IsSuccessStatusCode)
-                {
-                    var FeedbackOfProduct = await FeedbackOfProductResponse.Content.ReadFromJsonAsync<List<FeedbackForm>>();
-
-                    dashboard.FeedbackProduct = FeedbackOfProduct;
-                }
-
-                // đánh giá của khách hàng về các phòng
-                HttpResponseMessage FeedbackOfRoomResponse = await client.GetAsync("https://localhost:7255/api/Dashboard/GetFeedbackOfRoom");
-
-                if (FeedbackOfRoomResponse.IsSuccessStatusCode)
-                {
-                    var FeedbackOfRoom = await FeedbackOfRoomResponse.Content.ReadFromJsonAsync<List<FeedbackForm>>();
-
-                    dashboard.FeedbackRoom = FeedbackOfRoom;
-                }
-
-                // đánh giá của khách hàng về các dịch vụ
-                HttpResponseMessage FeedbackOfServiceResponse = await client.GetAsync("https://localhost:7255/api/Dashboard/GetFeedbackOfService");
-
-                if (FeedbackOfServiceResponse.IsSuccessStatusCode)
-                {
-                    var FeedbackOfService = await FeedbackOfServiceResponse.Content.ReadFromJsonAsync<List<FeedbackForm>>();
-
-                    dashboard.FeedbackService = FeedbackOfService;
-                }
             }
             catch (Exception ex)
             {
