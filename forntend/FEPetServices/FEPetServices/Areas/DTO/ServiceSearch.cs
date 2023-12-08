47d39f85ba7bc06b56779b943ab8978bd9425d26
linh2772001 <linhnguyenhihi2772001@gmail.com>
2023-12-08 17:21:47 +0700

    sửa lại trang dịch vụ trong home

--- forntend/FEPetServices/FEPetServices/Areas/DTO/ServiceSearch.cs
+++ forntend/FEPetServices/FEPetServices/Areas/DTO/ServiceSearch.cs
@@ -2,7 +2,13 @@
 {
     public class ServiceSearch
     {
+        public int? page { get; set; }
+        public int? pagesize { get; set; }
         public string? sortby { get; set; }
         public string? servicename { get; set; }
+        public string? sevicecategory { get; set; }
+        public string? pricefrom { get; set; }
+        public string? priceto { get; set; }
+  
     }
 }
